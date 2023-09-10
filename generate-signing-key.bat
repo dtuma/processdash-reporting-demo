c6cdcad2519306ca2a69e7d9398d6cd4da5719e3
David Tuma <processdash@tuma-solutions.com>
2023-09-10 09:01:23 -0600

    Quote the path to keytool to handle spaces in JAVA_HOME

--- generate-signing-key.bat
+++ generate-signing-key.bat
@@ -1,1 +1,1 @@
-%JAVA_HOME%\bin\keytool -genkey -keystore keystore -storepass changeit -keypass changeit -alias pdash-addon -keyalg RSA -keysize 4096 -validity 1096
+"%JAVA_HOME%\bin\keytool" -genkey -keystore keystore -storepass changeit -keypass changeit -alias pdash-addon -keyalg RSA -keysize 4096 -validity 1096
